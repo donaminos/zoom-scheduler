c31b38f1f4441d716116b0ba4dbb1458ed292ac1
Amine Sadry <amine.sadry@gmail.com>
2021-02-15 14:07:11 +0100

    feat: init project as Next.js app

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Zoom scheduler</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Zoom scheduler</h1>
+      </main>
+    </div>
+  );
+}
